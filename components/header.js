5ab292e209e82685d7c511f0a71114f997984558
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2025-04-04 12:38:39 -0300

    instalando bootstrap e alterando detalhes de imagem

--- components/header.js
+++ components/header.js
@@ -6,23 +6,23 @@ export default function Header() {
   return (
     <header>
       <div className="logo-fatec">
-        <Image src="/imgs/logo fatec.png" alt="Logo Fatec" width={100} height={50} className="logo-fatec-foto" />
+        <Image src="/imgs/logo fatec.png" alt="Logo Fatec" width={409} height={98} className="logo-fatec-foto" />
       </div>
       
       <div className="redes-sociais-icons"> 
         {/* posteriormente endereçar corretamente */}
-        <Link href="#"><Image src="/imgs/flickr.png" alt="Flickr" width={24} height={24} /></Link>
-        <Link href="#"><Image src="/imgs/linkedin.png" alt="Linkedin" width={24} height={24} /></Link>
-        <Link href="#"><Image src="/imgs/tiktok.png" alt="Tiktok" width={24} height={24} /></Link>
-        <Link href="#"><Image src="/imgs/youtube.png" alt="Youtube" width={24} height={24} /></Link>
-        <Link href="#"><Image src="/imgs/twitter.png" alt="Twitter" width={24} height={24} /></Link>
-        <Link href="#"><Image src="/imgs/istagram.png" alt="Instagram" width={24} height={24} /></Link>
-        <Link href="#"><Image src="/imgs/facebook.png" alt="Facebook" width={24} height={24} /></Link>
+        <Link href="#"><Image src="/imgs/flickr.png" alt="Flickr" width={30} height={30} /></Link>
+        <Link href="#"><Image src="/imgs/linkedin.png" alt="Linkedin" width={30} height={30} /></Link>
+        <Link href="#"><Image src="/imgs/tiktok.png" alt="Tiktok" width={30} height={30} /></Link>
+        <Link href="#"><Image src="/imgs/youtube.png" alt="Youtube" width={30} height={30} /></Link>
+        <Link href="#"><Image src="/imgs/twitter.png" alt="Twitter" width={30} height={30} /></Link>
+        <Link href="#"><Image src="/imgs/istagram.png" alt="Instagram" width={30} height={30} /></Link>
+        <Link href="#"><Image src="/imgs/facebook.png" alt="Facebook" width={30} height={30} /></Link>
         <Link href="#" className="governo"><p>/governosp</p></Link>
       </div>
 
       <div className="menu-hamburguer">
-        <Link href="#"><Image src="/imgs/hamburguer.png" alt="Menu" width={24} height={24} /></Link>
+        <Link href="#"><Image src="/imgs/hamburguer.png" alt="Menu" width={20} height={13} /></Link>
       </div>
     </header>
   )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
