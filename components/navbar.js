516b8b21ba6f8a7a0d0806a47d3572bad9e4043f
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2025-04-07 22:24:32 -0300

    Decorando a nav-bar

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,28 @@
+import '../src/styles/navbar.css'
 const Navbar = () => {
     return (
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <nav class="navbar navbar-expand-lg nav-padrao">
         <div class="container-fluid">
           <a class="navbar-brand" href="#">
-            logo cps
+            <img
+              class="logo-cps-55"
+              src="/imgs/2024_logo_55anos_cps_gov_24-25_regua_horizontal+horizontal_br 1.png"
+              alt=""
+              width={294.05}
+              height={71}
+            />
           </a>
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">
-                  logo fatec
+                  <img
+                    class="logo-fatec-registro"
+                    src="/imgs/fatec_ra_registro_registro_br.png"
+                    alt=""
+                    width={109.46}
+                    height={69}
+                  />
                 </a>
               </li>
               <li class="nav-item home-item">
@@ -30,17 +43,12 @@ const Navbar = () => {
                 <ul class="dropdown-menu">
                   <li>
                     <a class="dropdown-item" href="#">
-                      Action
+                      Usuário
                     </a>
                   </li>
                   <li>
                     <a class="dropdown-item" href="#">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">
-                      Something else here
+                      Projeto
                     </a>
                   </li>
                 </ul>
