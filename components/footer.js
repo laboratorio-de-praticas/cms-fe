c7cfd876c955d39293c95f9ff53c09880352181b
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2025-04-08 21:36:51 -0300

    Corrigindo imagem do footer

--- components/footer.js
+++ components/footer.js
@@ -1,11 +1,17 @@
 import Image from 'next/image'
-import '../src/styles/header.css' 
+import '../src/styles/footer.css' 
 
 const Footer=()=>{
-    return(
-      <div class="bg-dark d-flex justify-content-center">
-        <Image src="/imgs/logo_cps_gov.png" alt="Logo CPS Gov" width={409} height={98} className="logo-fatec-foto" />
+    return (
+      <div class="footer-fundo d-flex justify-content-center">
+        <Image
+          src="/imgs/logo_cps_gov.png"
+          alt="Logo CPS Gov"
+          width={279.4}
+          height={127}
+          className="logo-fatec-footer"
+        />
       </div>
-    )
+    );
 }
 export default Footer
