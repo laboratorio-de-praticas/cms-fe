e3f057032b4d4430a61541cea00ea826bc60023b
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2025-04-09 14:59:07 -0300

    Arrumando card de estudantes e adiciionando responsividade basica que pode ser mais elaborada mais pra frente quandoestiver pronto ux mobile

--- components/student-card.js
+++ components/student-card.js
@@ -9,25 +9,31 @@ const StudentCard = () => {
 
   return (
     <div className="container-fluid container-students">
-      <div className="student-card-wrapper">
-        {students.map((name, index) => (
-          <div className="student-card" key={index}>
-            <div className="student-info">
-              <div className="student-avatar"></div>
-              <div>
-                <div className="student-name">{name}</div>
-                <div className="student-class">DSM 4</div>
-              </div>
+        <div className="student-card-wrapper">
+            {students.map((name, index) => (
+            <div className="student-row" key={index}>
+                <div className="student-card">
+                <div className="student-info">
+                    <div className="student-avatar"></div>
+                    <div>
+                    <div className="student-name">{name}</div>
+                    <div className="student-class">DSM 4</div>
+                    </div>
+                </div>
+                <div className="student-actions">
+                    <img src="/imgs/arrow-student-card.svg" alt="seta" className="seta-card" />
+                </div>
+                </div>
+                <div className="student-buttons">
+                <img src="/imgs/edit-student.svg" alt="Editar" className="icon-button" />
+                <img src="/imgs/Delete-student.svg" alt="Deletar" className="icon-button" />
+                </div>
             </div>
-            <div className="student-actions">
-              <i className="bi bi-pencil-fill text-warning"></i>
-              <img src="/imgs/arrow-student-card.svg" alt="" className="seta-card"/>
-              <i className="bi bi-trash-fill text-danger"></i>
-            </div>
-          </div>
-        ))}
-      </div>
+            ))}
+        </div>
     </div>
+
+  
   );  
 };
 
