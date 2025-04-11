2f1e7cd7c64a4847d71850cad7dcd6780c912f00
Pedro Xavier <105009132+OxeMonoYuumi@users.noreply.github.com>
2025-04-10 22:21:43 -0300

    Add files via upload

---
+++ src/pages/form.js
@@ -0,0 +1,19 @@
+import Header from '../../components/header.js'
+import Forms from "../../components/forms";
+import Footer from '../../components/footer.js';
+import Navbar from '../../components/navbar.js';
+
+export default function Form(){
+    return(
+        <>
+        <Header />
+        <Navbar />
+        <main>
+            <br/>
+            <Forms />
+            <br/>
+        </main>
+        <Footer />
+        </>
+    )
+}
