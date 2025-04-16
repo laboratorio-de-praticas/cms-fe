adff5b2b96ff2763776bcf079a1ef8d47afc96c7
Pedro Xavier <105009132+OxeMonoYuumi@users.noreply.github.com>
2025-04-15 21:13:34 -0300

    Add files via upload

--- components/forms.js
+++ components/forms.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import '../src/styles/form.css'
 
 const Forms = () => {
     const [formData, setFormData] = useState({
@@ -66,27 +67,56 @@ const Forms = () => {
     };
 
     return (
-        <div className="container" style={{ color: '#004854' }}>
+        <div className="container" style={{ color: '#004854', border: 'solid', borderWidth:1, borderRadius:5}}>
             <div className="row">
                 <div className="col-md-4">
-                    <h5>Cadastro</h5>
-                    <h3 className='fw-bold'>Alunos</h3>
+                <br/>
+                <br/>
+                    <h5>Tela de cadastrado do</h5>
+                    <h3 className='fw-bold'>CMS - Sistema de</h3>
+                    <h3 className='fw-bold'>Gerenciamento de Conteúdo</h3>
                 </div>
-                <hr />
             </div>
-
+            <br/>
             <form className="g-3" method="POST" onSubmit={handleSubmit} encType="multipart/form-data">
+
+                {/* Candidato e Líder */}
                 <div className="row d-flex justify-content-center">
-                    <div className="col-md-3">
-                        <div className="btn-group grupo-1" role="group">
-                            <input type="radio" className="btn-check" name="curso" id="option1" autoComplete="off" value="DSM" onChange={handleCursoChange} />
-                            <label className="btn btn-secondary" htmlFor="option1">DSM</label>
-                            <input type="radio" className="btn-check" name="curso" id="option2" autoComplete="off" value="GE" onChange={handleCursoChange} />
-                            <label className="btn btn-secondary" htmlFor="option2">GE</label>
+                    <div className="col-md-6">
+                        <div className="input-group align-items-center" style={{height:61}}> 
+                            <span className="input-group-text fw-bold bg-white" style={{ color: '#004854' }}>Foto 3x4:</span>
+                            <input type="file" className="form-control border-start-0" name="foto" onChange={handleChange} required />
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="ms-0 row form-control">
+                            <div className="col-md-12">
+                                <div className="input-group gap-3">
+                                    <input className="form-check-input" type="checkbox" name="deseja_ser_candidato" checked={formData.deseja_ser_candidato} onChange={handleChange} /> 
+                                    <div className="fw-bold" style={{ color: '#004854' }}>Desejo me cadastrar como Representante de Turma</div>
+                                </div>
+                            </div>
+                            <div className="col-md-12">
+                                <div className="input-group gap-3">
+                                    <input className="form-check-input" type="checkbox" name="deseja_ser_lider" checked={formData.deseja_ser_lider} onChange={handleChange} />
+                                    <div className="fw-bold" style={{ color: '#004854' }} >Desejo me cadastrar como Líder do grupo PI</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <div className="row d-flex justify-content-center">
+                {/* Curso */}
+                    <div className="col-md-6">
+                        <div className="input-group" role="group">
+                        <span className="input-group-text fw-bold bg-white" style={{ color: '#004854'}}>Curso:</span>
+                        <input type="text" className="form-control border-start-0" name="curso" value={formData.curso} onChange={handleChange} placeholder="Ex: Desenvenvolvimento de Software Multiplataforma" required />
                         </div>
                     </div>
 
-                    <div className="col-md-3">
+                {/* Turma Atual */}
+                    <div className="col-md-6">
                         <div className="input-group">
                             <span className="input-group-text fw-bold bg-white" style={{ color: '#004854'}}>Semestre Atual:</span>
                             <input type="text" className="form-control border-start-0" name="turma_atual" value={formData.turma_atual} onChange={handleChange} placeholder="Ex: 5" required />
@@ -103,9 +133,35 @@ const Forms = () => {
                             <input type="text" className="form-control border-start-0" name="nome" value={formData.nome} onChange={handleChange} placeholder="Digite seu nome completo" required />
                         </div>
                     </div>
+
+                {/* Ano de Ingresso */}
+                <br />
+                    <div className="col-md-6">
+                        <div className="input-group">
+                            <span className="input-group-text fw-bold bg-white" style={{ color: '#004854' }}>Ano de Ingresso:</span>
+                            <input type="text" className="form-control border-start-0" name="ano_ingresso" value={formData.ano_ingresso} onChange={handleChange} placeholder="Ex: 2023 / 2" required />
+                        </div>
+                    </div>
                 </div>
 
-                {/* Email Institucional */}
+                {/* Senha e RA */}
+                <br />
+                <div className="row d-flex justify-content-center">
+                    <div className="col-md-6">
+                        <div className="input-group">
+                            <span className="input-group-text fw-bold bg-white" style={{ color: '#004854' }}>Senha:</span>
+                            <input type="password" className="form-control border-start-0" name="senha" value={formData.senha} onChange={handleChange} placeholder="Digite sua senha" required />
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="input-group">
+                            <span className="input-group-text fw-bold bg-white" style={{ color: '#004854' }}>RA:</span>
+                            <input type="text" className="form-control border-start-0" name="ra" value={formData.ra} onChange={handleChange} placeholder="Digite seu RA" required />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Email e Telefone */}
                 <br />
                 <div className="row d-flex justify-content-center">
                     <div className="col-md-6">
@@ -114,77 +170,24 @@ const Forms = () => {
                             <input type="email" className="form-control border-start-0" name="email_institucional" value={formData.email_institucional} onChange={handleChange} placeholder="Digite seu email@fatec.sp.gov.br" required />
                         </div>
                     </div>
-                </div>
-
-                {/* Telefone e RA */}
-                <br />
-                <div className="row d-flex justify-content-center">
-                    <div className="col-md-3">
+                    <div className="col-md-6">
                         <div className="input-group">
                             <span className="input-group-text fw-bold bg-white" style={{ color: '#004854' }}>Celular:</span>
                             <input type="text" className="form-control border-start-0" name="telefone" value={formData.telefone} onChange={handleChange} placeholder="(00) 00000 - 0000" required />
-                        </div>
-                    </div>
-                    <div className="col-md-3">
-                        <div className="input-group">
-                            <span className="input-group-text fw-bold bg-white" style={{ color: '#004854' }}>RA:</span>
-                            <input type="text" className="form-control border-start-0" name="ra" value={formData.ra} onChange={handleChange} placeholder="Digite seu RA" required />
-                        </div>
-                    </div>
-                </div>
-
-                {/* Ano de Ingresso e Senha */}
-                <br />
-                <div className="row d-flex justify-content-center">
-                    <div className="col-md-3">
-                        <div className="input-group">
-                            <span className="input-group-text fw-bold bg-white" style={{ color: '#004854' }}>Ano de Ingresso:</span>
-                            <input type="text" className="form-control border-start-0" name="ano_ingresso" value={formData.ano_ingresso} onChange={handleChange} placeholder="Ex: 2023 / 2" required />
-                        </div>
-                    </div>
-                    <div className="col-md-3">
-                        <div className="input-group">
-                            <span className="input-group-text fw-bold bg-white" style={{ color: '#004854' }}>Senha:</span>
-                            <input type="password" className="form-control border-start-0" name="senha" value={formData.senha} onChange={handleChange} placeholder="Digite sua senha" required />
                         </div>
                     </div>
                 </div>
 
                 {/* Foto */}
                 <br />
-                <div className="row d-flex justify-content-center">
-                    <div className="col-md-6">
-                        <div className="input-group">
-                            <span className="input-group-text fw-bold bg-white" style={{ color: '#004854' }}>Foto 3x4:</span>
-                            <input type="file" className="form-control border-start-0" name="foto" onChange={handleChange} required />
-                        </div>
+                <div className="row d-flex justify-content-end">
+                    <div className='col-md-6 d-flex justify-content-between'>
+                        <button type="submit" className="btn btn-success col-md-7">Cadastrar</button>
+                        <a href='/' className="btn btn-outline-danger d-flex justify-content-center align-items-center col-md-4">Cancelar</a>
                     </div>
-                </div>
-
-                {/* Candidato e Líder */}
                 <br />
-                <div className="row d-flex justify-content-center">
-                    <div className="col-md-6">
-                        <div className="input-group gap-3">
-                            <div className="fw-bold">Desejo me cadastrar como Representante de Turma</div>
-                            <input className="form-check-input" type="checkbox" name="deseja_ser_candidato" checked={formData.deseja_ser_candidato} onChange={handleChange} />
-                        </div>
-                    </div>
                 </div>
                 <br />
-                <div className="row d-flex justify-content-center">
-                    <div className="col-md-6">
-                        <div className="input-group gap-3">
-                            <div className="fw-bold">Desejo me cadastrar como Líder do grupo PI</div>
-                            <input className="form-check-input" type="checkbox" name="deseja_ser_lider" checked={formData.deseja_ser_lider} onChange={handleChange} />
-                        </div>
-                    </div>
-                </div>
-
-                <br />
-                <div className="row d-flex justify-content-center">
-                    <button type="submit" className="btn btn-success col-md-6">Cadastrar</button>
-                </div>
             </form>
         </div>
     );
