89331f5058ad5fc0d58d2e1e9a77c01d292a4de9
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2025-04-16 20:32:10 -0300

    Atualizando menu de perfil de acordo com novo figma

--- components/menu-profile.js
+++ components/menu-profile.js
@@ -1,49 +1,59 @@
 import "../src/styles/menu-profile.css";  
 
 const MenuProfile = ({ menuAberto, fecharMenu }) => {
-    return(
-        <>
-            {/* sombra */}
-            {menuAberto && (
-                <div
-                    className="shadow-overlay"
-                    onClick={fecharMenu}
-                ></div>
-            )}
+    return (
+      <>
+        {/* sombra */}
+        {menuAberto && (
+          <div className="shadow-overlay" onClick={fecharMenu}></div>
+        )}
 
-            <div className={`menu-perfil-container ${menuAberto ? "aberto" : ""}`}>
-                <div className="row">
-                    <img src="/imgs/close-x.svg" className="close-menu" width={20} height={20} alt="Fechar" onClick={fecharMenu} />
-                </div>
-                <div className="row">
-                    <div className="col-md">
-                        <img className="foto-perfil-menu" src="imgs/foto-perfil.png" alt="" />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="profile-description">
-                        <p className="nome-perfil-menu">Calebe Cardoso Almeida Pereira</p>
-                        <p className="curso-perfil-menu">DSM 4</p>
-                        <p className="tipo-usuarioperfil-menu">ADM</p>
-                    </div>
-                </div>
-                <div className="row">
-                    <button className="editar-perfil">Editar Perfil</button>
-                </div>
-                <div className="row">
-                    <button className="sair-perfil">Sair Perfil</button>
-                </div>
-                <div className="row">
-                    <div className="profile-divider"></div>
-                </div>
-                <div className="row">
-                    <button className="cadastrar-projeto">Cadastrar Projeto</button>
-                </div>
-                <div className="row">
-                    <button className="editar-projeto">Editar Projeto</button>
-                </div>
+        <div className={`menu-perfil-container ${menuAberto ? "aberto" : ""}`}>
+          <div className="row">
+            <img
+              src="/imgs/close-x.svg"
+              className="close-menu"
+              width={20}
+              height={20}
+              alt="Fechar"
+              onClick={fecharMenu}
+            />
+          </div>
+          <div className="row">
+            <div className="col-md">
+              <img
+                className="foto-perfil-menu"
+                src="imgs/foto-perfil.png"
+                alt=""
+              />
             </div>
-        </>
+          </div>
+          <div className="row">
+            <div className="profile-description">
+              <p className="nome-perfil-menu">Calebe Cardoso Almeida Pereira</p>
+              <p className="curso-perfil-menu">DSM 4</p>
+              <p className="tipo-usuarioperfil-menu">Aluno - Líder</p>
+              <div className="profile-divider"></div>
+              <p className="nome-grupo-menu">Learny</p>
+              <p className="descgrupo-menu">Ensino de Inglês Gamificado para crianças com TEA.</p>
+              <div className="integrantes">
+                {/* aqui devera ter um calculo de proporcionalidade de acordo qtd de integrantes para as fotos 
+                ficarem na mesma linha  */}
+                <img src="/imgs/foto-perfil.png" width={50} height={50} alt="" />
+                <img src="/imgs/foto-perfil2.png" width={50} height={50} alt="" />
+                <img src="/imgs/foto-perfil.png" width={50} height={50} alt="" />
+                <img src="/imgs/foto-perfil2.png" width={50} height={50} alt="" />
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <button className="editar-perfil">Editar Perfil</button>
+          </div>
+          <div className="row">
+            <button className="sair-perfil">Sair Perfil</button>
+          </div>
+        </div>
+      </>
     );
 }
 
