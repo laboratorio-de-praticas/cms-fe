c27860450f567633d175b22039014d18e8af9b9d
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2025-04-22 01:16:46 -0300

    atualizando as options da visao adm

--- components/index-options.js
+++ components/index-options.js
@@ -13,29 +13,38 @@ const IndexOptions = () => {
       <div className="row justify-content-start">
         <div className="col-auto">
           <button
-            className="btn-option primary"
-            onClick={() => handleRedirect('/loginUser')} // Redireciona para /loginUser
-          >
-            <img src="/imgs/entrar.svg" alt="" width={90} height={90} />
-            <span>Entrar</span>
-          </button>
-        </div>
-        <div className="col-auto">
-          <button
-            className="btn-option outlined"
-            onClick={() => handleRedirect('/formStudent')}
-          >
-            <img src="/imgs/cadastro-aluno.svg" alt="" width={90} height={90} />
-            <span>Cadastro Aluno</span>
-          </button>
-        </div>
-        <div className="col-auto">
-          <button
-            className="btn-option outlined"
+            className="btn-option visitante"
             onClick={() => handleRedirect('/cadVisitante')} 
           >
-            <img src="/imgs/cadastro-visitante.svg" alt="" width={90} height={90} />
+            <img src="/imgs/cadastro-visitante-branco.svg" alt="" width={90} height={90} />
             <span>Cadastro Visitante</span>
+          </button>
+        </div>
+        <div className="col-auto">
+          <button
+            className="btn-option sistema"
+            onClick={() => handleRedirect('/cadVisitante')} 
+          >
+            <img src="/imgs/entrar-sistema.svg" alt="" width={90} height={90} />
+            <span>Entrar Sistema</span>
+          </button>
+        </div>
+        <div className="col-auto">
+          <button
+            className="btn-option pedidos"
+            onClick={() => handleRedirect('/cadVisitante')} 
+          >
+            <img src="/imgs/gerenciar-pedidos.svg" alt="" width={90} height={90} />
+            <span>Gerenciar Pedidos</span>
+          </button>
+        </div>
+        <div className="col-auto">
+          <button
+            className="btn-option eventos"
+            onClick={() => handleRedirect('/cadVisitante')} 
+          >
+            <img src="/imgs/ver-eventos.svg" alt="" width={90} height={90} />
+            <span>Ver eventos</span>
           </button>
         </div>
       </div>
