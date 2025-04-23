dcc8ad25f9286233053b9e3e8cd3e296a9148a1a
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2025-04-23 15:59:27 -0300

    atualizacao final

--- context/userContext.js
+++ context/userContext.js
@@ -4,7 +4,7 @@ import { createContext, useContext, useState } from "react";
 const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [userType, setUserType] = useState("aluno"); // valor padrão pode vir da API, login, etc
+  const [userType, setUserType] = useState("adm"); // valor padrão pode vir da API, login, etc
 
   return (
     <UserContext.Provider value={{ userType, setUserType }}>
