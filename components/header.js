1dca811bed6b625295a0b6aa2a6f44c65edf3818
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2025-04-23 20:20:39 -0300

    hamburguer header redes sociais mobile

--- components/header.js
+++ components/header.js
@@ -1,30 +1,43 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import '../src/styles/header.css' 
+import { useState } from 'react'
+import '../src/styles/header.css';
 
 export default function Header() {
+   const [menuAbertoHeader, setMenuAbertoHeader] = useState(false)
+
+  const toggleMenuHeader = () => {
+    setMenuAbertoHeader(!menuAbertoHeader)
+  }
   return (
     <header>
       <div className="logo-fatec">
-        <Image src="/imgs/logo fatec.png" alt="Logo Fatec"width={327.2} height={78.4} className="logo-fatec-foto" />
+        <Image
+          src="/imgs/logo fatec.png"
+          alt="Logo Fatec"
+          width={327.2}
+          height={78.4}
+          className="logo-fatec-foto"
+        />
       </div>
-      
-      <div className="redes-sociais-icons"> 
-        {/* posteriormente endereçar corretamente */}
-        <Link href="#"><Image src="/imgs/flickr.png" alt="Flickr" width={30} height={30} /></Link>
+
+      <div className={`redes-sociais-icons ${menuAbertoHeader ? 'ativo' : ''}`}>
+        <Link href="https://www.flickr.com/photos/governosp/">
+          <Image src="/imgs/flickr.png" alt="Flickr" width={30} height={30} />
+        </Link>
         <Link href="#"><Image src="/imgs/linkedin.png" alt="Linkedin" width={30} height={30} /></Link>
         <Link href="#"><Image src="/imgs/tiktok.png" alt="Tiktok" width={30} height={30} /></Link>
         <Link href="#"><Image src="/imgs/youtube.png" alt="Youtube" width={30} height={30} /></Link>
         <Link href="#"><Image src="/imgs/twitter.png" alt="Twitter" width={30} height={30} /></Link>
         <Link href="#"><Image src="/imgs/istagram.png" alt="Instagram" width={30} height={30} /></Link>
         <Link href="#"><Image src="/imgs/facebook.png" alt="Facebook" width={30} height={30} /></Link>
-        <Link href="#" className="governo"><p>/governosp</p></Link>
+        <Link href="https://www.governo.sp.gov.br/" className="governo"><p>/governosp</p></Link>
       </div>
 
-      <div className="menu-hamburguer">
-        <Link href="#"><Image src="/imgs/hamburguer.png" alt="Menu" width={20} height={13} /></Link>
+      <div className="menu-hamburguer" onClick={toggleMenuHeader}>
+        <Image src="/imgs/hamburguer.png" alt="Menu" width={20} height={13} />
       </div>
     </header>
-  )
+  );
 }
 // autora: Isabele Letícia
