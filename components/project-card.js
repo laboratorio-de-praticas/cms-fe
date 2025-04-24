bc2c204d1a4a5fc32b21767a3a0f64b66eca7a42
IsabeleLeticiaQueiroz <bbelequeiroz@gmail.com>
2025-04-24 16:28:54 -0300

    card de projetos minimizado

--- components/project-card.js
+++ components/project-card.js
@@ -1,29 +1,40 @@
 import "../src/styles/project-card.css";
 import React from "react";
 
-const PorjectCard = () => {
+const ProjectCard = () => {
   return (
-    <div className="container-md container-project-card">
-        {/* minimizado */}
-        <div className="card-project-minimizado">
-            <div className="project-img">
-                <img src="/img/project-img.png" width={120} height={120} alt="" />
+    <div className="container-project-card">
+      <div className="card-project-minimizado">
+        <div className="project-img">
+          <img src="/imgs/project-img.png" alt="Logo do projeto" />
+        </div>
+
+        <div className="infos-proj">
+        <div className="top-info">
+            <div className="titulo-e-turma">
+                <h4 className="nome">Kastle - Learny</h4>
+                <p className="turma">DSM 4</p>
             </div>
-            <div className="infos-proj">
-                <div className="row">
-                    <div className="nome">Kastle - Learny</div>
-                    <div className="status-e-btns">
-                        <p className="status">Ativo</p>
-                        <button className="editar">Editar</button>
-                        <button className="deletar">Deletar</button>
-                    </div>
-                    <div className="expandir">
-                        <img src="/imgs/arrow-student-card.svg" alt="" />
-                    </div>
+
+            <div className="acoes">
+                <span className="status ativo">Ativo</span>
+                <button className="editar">Editar</button>
+                <button className="deletar">Deletar</button>
+                <div className="expandir">
+                <img src="/imgs/arrow-student-card.svg" alt="Expandir" />
                 </div>
             </div>
+            </div>
+
+            <div className="members">
+            <img src="/imgs/foto-perfil.png" width={50} height={50}  alt="Membro 1" />
+            <img src="/imgs/foto-perfil2.png" width={50} height={50} alt="Membro 2" />
+            <img src="/imgs/foto-perfil2.png" width={50} height={50} alt="Membro 3" />
+            </div>
         </div>
+      </div>
     </div>
   );
-}
-export default PorjectCard;
+};
+
+export default ProjectCard;
